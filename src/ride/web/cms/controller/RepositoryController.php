8ac10c03512290980dc0a7eefb5f3dd262551d73
Joris Vandeweerd <joris@statik.be>
2014-03-21 15:30:11 +0100

    initial commit

---
+++ src/ride/web/cms/controller/RepositoryController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ride\web\cms\controller;
+
+use ride\web\base\controller\AbstractController;
+use ride\web\cms\VcsApplicationListener;
+
+/**
+ * Controller to handle the content repository of the CMS
+ */
+class RepositoryController extends AbstractController {
+
+    /**
+     * Action to show the last commits
+     * @param  \ride\web\cms\VcsApplicationListener $vcs Instance of the version
+     * control application listener
+     * @return null
+     */
+    public function indexAction(VcsApplicationListener $vcs) {
+        $repository = $vcs->getRepository();
+
+        $commits = $repository->getCommits(null, 10);
+
+        $this->setTemplateView('cms/backend/repository', array(
+            'commits' => $commits,
+        ));
+    }
+
+    /**
+     * Action to update the repository
+     * @param  \ride\web\cms\VcsApplicationListener $vcs Instance of the version
+     * control application listener
+     * @return null
+     */
+    public function updateAction(VcsApplicationListener $vcs) {
+        $vcs->updateRepository();
+
+        $referer = $this->request->getQueryParameter('referer');
+        if (!$referer) {
+            $referer = $this->getUrl('cms.repository');
+        }
+
+        $this->response->setRedirect($referer);
+    }
+
+}
